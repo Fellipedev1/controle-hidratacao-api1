c57b7aa3f659aa6916a86ec0ca67440afdcdf0e3
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-10-27 13:40:56 -0300

    att

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3080;
 
 app.use(express.json());
-app.use(cors()); // Habilita o CORS para todas as rotas
+app.use(cors());
 
 let registrosDeHidratacao = [];
 
-// Rota para obter todos os registros de hidratação
 app.get('/registros', (req, res) => {
   res.json(registrosDeHidratacao);
 });
 
-// Rota para adicionar um novo registro de hidratação
 app.post('/registros', (req, res) => {
   const novoRegistro = req.body;
   registrosDeHidratacao.push(novoRegistro);
   res.json(novoRegistro);
 });
 
-const ipAddress = ''; // Substitua 'SEU_ENDERECO_IP_LOCAL' pelo seu endereço IP local
-app.listen(port, ipAddress, () => {
-  console.log(`Servidor rodando na porta ${port} e no endereço IP ${ipAddress}`);
-});
+module.exports = app;
