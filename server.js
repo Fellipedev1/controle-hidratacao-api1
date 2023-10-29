3dd03a9b0accb0930cffbbb9e5316be7e5c3324b
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-10-28 23:37:30 -0300

    att

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000; // Porta alterada para 3000
+const port = process.env.PORT || 3000; // Alterado para usar a variável de ambiente PORT ou 3000 se não estiver definida
 
 app.use(express.json());
 app.use(cors());
@@ -19,7 +18,7 @@ app.post('/registros', (req, res) => {
   res.json(novoRegistro);
 });
 
-const ipAddress = '192.168.0.101'; // Você pode deixar assim para escutar em todas as interfaces de rede
+const ipAddress = '0.0.0.0'; // Escuta em todas as interfaces de rede
 app.listen(port, ipAddress, () => {
   console.log(`Servidor rodando na porta ${port} e no endereço IP ${ipAddress}`);
 });
